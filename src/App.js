4053c1f8e8967348217f62a32ee4deb3ad37870d
Kipeo <kp225048@gmail.com>
2024-02-08 15:17:31 +0900

    tutorial1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import { useState, useRef } from "react";
 import TodoList from "./TodoList";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const [todos, setTodo] = useState([]);
+
+  const todoNameuseRef = useRef();
+
+  const handleAddTodo = () => {
+    // タスクを追加
+    const name = todoNameuseRef.current.value;
+    if (name === "") return;
+    setTodo((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
+    });
+    todoNameuseRef.current.value = null;
+  };
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos]; //newTodosに一旦コピー
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodo(newTodos);
+  };
+
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodo(newTodos);
+  };
+
   return (
     <div>
-      <TodoList />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input type="text" ref={todoNameuseRef} />
+      <button onClick={handleAddTodo}>タスクを追加</button>
+      <button onClick={handleClear}>完了したタスクの削除</button>
+      <div>残りのタスク: {todos.filter((todo) => !todo.completed).length}</div>
     </div>
   );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,11 @@
 // rafceで関数のようなコンポーネントを作る
 import React from "react";
+import Todo from "./Todo";
 
-const TodoList = () => {
-  return <div>TodoList</div>;
+const TodoList = ({ todos, toggleTodo }) => {
+  return todos.map((todo) => (
+    <Todo todo={todo} key={todo.id} toggleTodo={toggleTodo} />
+  ));
 };
 
 export default TodoList; //どのファイルでも使えるように
